9de5d6892ca9cc70aa7ae12ca37addb228286f39
Oliver Zhang <ozz@andrew.cmu.edu>
2019-02-16 23:01:49 +0000

    Fixed teleop launch file

--- src/MotorNode.py
+++ src/MotorNode.py
@@ -6,14 +6,17 @@ from std_msgs.msg import Float32MultiArray
 
 port_name = "/dev/ttyS0"
 baud_rate = 9600
+max_speed = 1
 
 def callback(data):
+    global controller
     for motor in range(4):
-        rospy.loginfo(rospy.get_caller_id() + " says set motor %s to speed %s", motor,data.data[motor])
-        controller.set_target_speed(motor, data.data[i])
+        speed = int(round(data.data[motor]/max_speed * 3200))
+        rospy.loginfo(rospy.get_caller_id() + " says set motor %d to %d (%.4f rad/s)", motor,speed,data.data[motor])
+        controller.set_target_speed(motor, speed)
     
 def init_control():
-
+    global controller
     controller = MotorController(port_name, baud_rate, debug=True)
     controller.safe_start_all()
     rospy.init_node('motor_control')
