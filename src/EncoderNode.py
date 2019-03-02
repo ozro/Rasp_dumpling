a9b2c1f44293d4117d312b90c523ed28c7b5a4cb
Oliver Zhang <ozz@andrew.cmu.edu>
2019-03-02 21:54:56 +0000

    Changed motor zero encoder pins

--- src/EncoderNode.py
+++ src/EncoderNode.py
@@ -5,9 +5,9 @@ import rospy
 from std_msgs.msg import Int16MultiArray
 
 # Channel A pins (yellow)
-Apins = [17, 19, 16 ,6]
+Apins = [27, 19, 16 ,6]
 # Channel B pins (green)
-Bpins = [27, 26, 20, 13]
+Bpins = [22, 26, 20, 13]
 
 # Channel A state
 Astate = [False] * 4
@@ -19,6 +19,7 @@ count = [0] * 4
 
 def callback(channel):
     pin = Apins.index(channel)
+    rospy.loginfo("Callback on channel %d pin %d", channel, pin)
     if(GPIO.input(Bpins[pin])):
         count[pin] -= 1
     else:
@@ -31,12 +32,12 @@ def init_node():
     for i in range(4):
         GPIO.setup(Apins[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(Bpins[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(Apins[i], GPIO.FALLING, callback=callback)  
 
-    GPIO.add_event_detect(Apins[i], GPIO.FALLING, callback=callback)  
 
     rospy.init_node('encoder_reader')
-    pub = rospy.Publisher("/motor_status/encoder_counts", Int16MultiArray, queue_size=10)
-    rate = rospy.Rate(10)
+    pub = rospy.Publisher("/motor_status/encoder_counts", Int16MultiArray, queue_size=1)
+    rate = rospy.Rate(1000)
     while not rospy.core.is_shutdown():
         array = Int16MultiArray()
         array.data = count
